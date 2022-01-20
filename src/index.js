e148f9b7b64325b3bc943277208cf5d77d845388
NanaValencia <diseno.1988@gmail.com>
2022-01-20 11:29:46 -0500

    Actualización funcion obtenerTC

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 //import validator from './validator.js';
 
-document.getElementById('Comprar').addEventListener('click', obtenerNumTC);
+//Elementos
+let btnComprar = document.getElementById("Comprar");
 
-function obtenerNumTC(){
-    let numeroTC = document.getElementById('numeroTC').value;
-    console.log(numeroTC)
+//Funciones
+function obtenerTc(event) {
+    event.preventDefault();
+    let numeroTC = document.getElementById("numeroTC").value;
+    console.log(numeroTC);
 }
 
-function mostrarTienda(){
-    document.getElementById('ProcesarCompra').style.display='block';
-}
-
-
-// document.getElementById('Pagar').addEventListener('click', displayComprar);
-
-// function displayComprar () {
-//     document.getElementById('ProcesarCompra').style.display='block';
-
-
+//Eventos
+btnComprar.addEventListener("click", obtenerTc)
