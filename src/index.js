b9e4e3ca4f16f9679b5c6761852476b3c169c58d
FRANCY15 <90322713+FRANCY15@users.noreply.github.com>
2022-01-21 12:49:33 -0500

    Avance función isValid, ocultar y mostrar tienda, corrección de estilos

--- src/index.js
+++ src/index.js
@@ -7,40 +7,35 @@ let btnPagar = document.getElementById("Pagar");
 
 //Funciones
 
-// let frutas = ['Manzana', 'Pera', 'Naranja', 'Papaya'];
-// let frutasInvertidas = frutas.reverse();
-// let longitud = frutas.length;
-// console.log(longitud);
-
-// let numeroTC = [];
-// numeroTC = document.getElementById('numeroTC').value;
-// let numeroInvertido = numeroTC.reverse();
-// console.log(numeroInvertido);
-    
-
-function obtenerTc(event) {
-    event.preventDefault();
+function isValid() {
+    // event.preventDefault();
     let numeroTC = document.getElementById("numeroTC").value;
     let cadena = numeroTC.toString();
     let longitud = cadena.length;
     let cifra = null;
     let cifra_cad = null;
     let suma = 0;
-    for (var i=0; i < longitud; i+2){
-        cifra=parseInt(cadena.charAt(i))*2;
-        if (cifra>9){
+    let numero = [];
+    for (var i=0; i < longitud; i++){
+        numero.push(cadena[i])        
+    }
+    let numeroInvertido = numero.reverse();
+    let longitud1 = numeroInvertido.length;
+    for(var x=0; x < longitud1; x++){
+        cifra = parseInt(numeroInvertido[x])*2;
+        if(cifra>9){
             cifra_cad=cifra.toString();
             cifra=parseInt(cifra_cad.charAt(0))+parseInt(cifra_cad.charAt(1));
         }
         suma += cifra;
     }
-    for (var x=1; x<longitud; x+2){
-        suma+= parseInt(cadena.charAt(i));
+    for (var y=1; y<longitud; y++){
+        suma+= parseInt(numeroInvertido[y]);
     }
     if ((suma % 10) == 0){
-        console.log('Gracias por tu compra');
+        alert('Gracias por tu compra');
     }else{
-        console.log('Transacción rechaza, verifique sus datos.')
+        alert('Transacción rechaza, verifique sus datos.')
     }
 }
 
@@ -48,13 +43,12 @@ function obtenerTc(event) {
 
 function portalPagos() {
     document.getElementById("ProcesarCompra").style.display = "block";
-}
-function ocultarTienda (){
     document.getElementById('tienda').style.display = 'none';
 }
+
 //Eventos
-btnComprar.addEventListener("click", obtenerTc);
-btnPagar.addEventListener("click", portalPagos, ocultarTienda);
+btnComprar.addEventListener("click", isValid);
+btnPagar.addEventListener("click", portalPagos);
 
 
 
