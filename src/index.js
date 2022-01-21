f27ea7dff381f653c64ed8802811e96deb688805
FRANCY15 <90322713+FRANCY15@users.noreply.github.com>
2022-01-21 10:29:12 -0500

    Ajuste hoja de estilos, función isValid, y modificación index.html

--- src/index.js
+++ src/index.js
@@ -1,14 +1,66 @@
 //import validator from './validator.js';
 
+
 //Elementos
 let btnComprar = document.getElementById("Comprar");
+let btnPagar = document.getElementById("Pagar");
 
 //Funciones
+
+// let frutas = ['Manzana', 'Pera', 'Naranja', 'Papaya'];
+// let frutasInvertidas = frutas.reverse();
+// let longitud = frutas.length;
+// console.log(longitud);
+
+// let numeroTC = [];
+// numeroTC = document.getElementById('numeroTC').value;
+// let numeroInvertido = numeroTC.reverse();
+// console.log(numeroInvertido);
+    
+
 function obtenerTc(event) {
     event.preventDefault();
     let numeroTC = document.getElementById("numeroTC").value;
-    console.log(numeroTC);
+    let cadena = numeroTC.toString();
+    let longitud = cadena.length;
+    let cifra = null;
+    let cifra_cad = null;
+    let suma = 0;
+    for (var i=0; i < longitud; i+2){
+        cifra=parseInt(cadena.charAt(i))*2;
+        if (cifra>9){
+            cifra_cad=cifra.toString();
+            cifra=parseInt(cifra_cad.charAt(0))+parseInt(cifra_cad.charAt(1));
+        }
+        suma += cifra;
+    }
+    for (var x=1; x<longitud; x+2){
+        suma+= parseInt(cadena.charAt(i));
+    }
+    if ((suma % 10) == 0){
+        console.log('Gracias por tu compra');
+    }else{
+        console.log('Transacción rechaza, verifique sus datos.')
+    }
 }
 
+
+
+function portalPagos() {
+    document.getElementById("ProcesarCompra").style.display = "block";
+}
+function ocultarTienda (){
+    document.getElementById('tienda').style.display = 'none';
+}
 //Eventos
-btnComprar.addEventListener("click", obtenerTc)
+btnComprar.addEventListener("click", obtenerTc);
+btnPagar.addEventListener("click", portalPagos, ocultarTienda);
+
+
+
+
+
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,32 @@
 const validator = {
-  // ...
+  
+  function isValid (numeroTC){
+    let numeroTC = [];
+    numeroTC = document.getElementById("numeroTC").value;
+    let cadena = numeroTC.toString();
+    let longitud = cadena.length;
+    let cifra = null;
+    let cifra_cad = null;
+    let suma = 0;
+    for (var i=0; i < longitud; i+2){
+        cifra=parseInt(cadena.charAt(i))*2;
+        if (cifra>9){
+            cifra_cad=cifra.toString();
+            cifra=parseInt(cifra_cad.charAt(0))+parseInt(cifra_cad.charAt(1));
+        }
+        suma += cifra;
+    }
+    for (var x=1; x<longitud; x+2){
+        suma+= parseInt(cadena.charAt(i));
+    }
+    if ((suma % 10) == 0){
+        alert('Gracias por tu compra');
+    }else{
+        alert('Transacción rechaza, verifique sus datos.')
+    }
+    
+
+}
 };
 
 export default validator;
